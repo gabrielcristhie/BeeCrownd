5973a5c9dd1db97fbd1a46d113fa03e93b7b8813
gabrielcristhie <gcristhie@hotmail.com>
2022-06-12 01:44:17 -0300

    coordenadas de um ponto

---
+++ src/Iniciante/coordenadasDeUmPonto.java
@@ -0,0 +1,37 @@
+package Iniciante;
+
+/*
+Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+
+Se o ponto estiver na origem, escreva a mensagem “Origem”.
+
+Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+
+Entrada
+A entrada contem as coordenadas de um ponto.
+
+Saída
+A saída deve apresentar o quadrante em que o ponto se encontra.
+*/
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class coordenadasDeUmPonto {
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.0");
+		
+		double x = sc.nextDouble();
+		double y = sc.nextDouble();
+		double me;
+			
+
+
+	
+		System.out.printf(" "+ df.format() + "\n");
+		
+		sc.close(); 
+	}
+}
